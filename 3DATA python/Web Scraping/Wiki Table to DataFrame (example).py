0a0a44b90d76476a8a264e7c9eaaadb95a84eb0e
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-09-01 08:53:58 +0200

    Add files via upload

---
+++ 3DATA python/Web Scraping/Wiki Table to DataFrame (example).py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+URL = "https://en.wikipedia.org/wiki/Characters_in_the_Animal_Crossing_series"
+table_class = "wikitable sortable jquery-tablesorter"
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+table = soup.find("table", {"class": "wikitable"})
+#  table = soup.find("table", {"id": "the_id"})
+
+df = pd.read_html(str(table))
+df = pd.DataFrame(df[0])
+print(df)
+
