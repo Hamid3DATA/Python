f503398af83e195e51480cce7006c815ba069b77
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-09-13 10:34:17 +0200

    Update guess age by name - ui.py

--- 01.08.2023-31.12.2023/API/guess age by name - ui.py
+++ 01.08.2023-31.12.2023/API/guess age by name - ui.py
@@ -24,6 +24,11 @@ print("")
 
 user_guess = input("How old do you think this person is?")
 
+if user_guess > age:
+    age_difference = int(user_guess) - int(age)
+elif age > user_guess:
+    age_difference = int(age) - int(user_guess)
+
 if user_guess == age:
     print("")
     print("CORRECT")
@@ -31,4 +36,12 @@ if user_guess == age:
 else:
     print("")
     print("INCORRECT")
+    print("")
     print(name + " is actually " + age + " years old!")
+    
+    if user_guess > age:
+      print("")
+      print("you were off by " + str(age_difference) + " year(s)")
+    elif age > user_guess:
+      print("")
+      print("you were off by " + str(age_difference) + " year(s)")
