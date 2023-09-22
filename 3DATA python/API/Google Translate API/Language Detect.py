fa06205b818390d68edfc2cd7de4c3ab97fef95b
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-09-22 12:45:30 +0200

    Update Language Detect.py

--- 3DATA python/API/Google Translate API/Language Detect.py
+++ 3DATA python/API/Google Translate API/Language Detect.py
@@ -3,7 +3,8 @@ import json
 
 url = "https://google-translate1.p.rapidapi.com/language/translate/v2/detect"
 
-payload = "q= hello, i can speak"
+text = input("Type inn anything in any language")
+payload = "q= " + text
 headers = {
 	"content-type": "application/x-www-form-urlencoded",
 	"Accept-Encoding": "application/gzip",
@@ -12,5 +13,18 @@ headers = {
 }
 
 response = requests.request("POST", url, data=payload, headers=headers)
-language = json.dumps(response.json())
-print(language)
+
+basic_response = json.dumps(response.json())
+
+language = json.dumps(response.json()["data"]["detections"][0][0]["language"])
+language = language.replace('"', '')
+
+confidence = json.dumps(response.json()["data"]["detections"][0][0]["confidence"])
+
+
+print("language: " + language)
+print("confidence: " + confidence)
+print("")
+print("")
+print("")
+print(basic_response)
