18d5ed2a042ed75654db376f9f69bebac0447852
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-09-28 13:00:27 +0200

    Update QR code generator.py

--- 01.08.2023-31.12.2023/API/QR code generator.py
+++ 01.08.2023-31.12.2023/API/QR code generator.py
@@ -2,10 +2,12 @@ import requests
 import json
 import webbrowser
 
-data = input("print the data you want to be in the QR code")
+print("print the data you want to be in the QR code")
+data = input()
 
+print("select the size of the QR Code in pixels (It's gonna be a square, so you select one side size onlyexample: 400)")
 try:
-  size = int(input("select the size of the QR Code in pixels (It's gonna be a square, so you select one side size onlyexample: 400)"))
+  size = int(input())
 except ValueError:
   print("it has to be a numebr!")
   exit()
@@ -13,5 +15,6 @@ except ValueError:
 url = "https://api.qrserver.com/v1/create-qr-code/?data=" + data + "&size=" + str(size) + "x" + str(size)
 
 webbrowser.open(url)
+print("")
 print("if a page with the QR code didn't open by itself, go to this link:")
 print(url)
