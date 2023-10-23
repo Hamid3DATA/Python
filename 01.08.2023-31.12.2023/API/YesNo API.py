a2892c6cca80a671217241e955e4f1b2cd94c400
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-10-23 14:47:55 +0200

    Update YesNo API.py

--- 01.08.2023-31.12.2023/API/YesNo API.py
+++ 01.08.2023-31.12.2023/API/YesNo API.py
@@ -4,6 +4,9 @@ import webbrowser
 r = requests.request("GET", "https://yesno.wtf/api")
 
 url = r.json()["image"]
+answer = r.json()["answer"]
 
 webbrowser.open(url)
+print(answer)
+print("")
 print(url)
